df150be754dca76aa4b2eedf61d95b0d45c7ca17
darpserj <sergiupopa999@outlook.com>
2022-12-05 21:02:45 +0200

    outside api

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
 import BookCreate from "./components/BookCreate"
 import BookList from './components/BookList'
 import "./index.css"
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 function App (){
 
     const [books, setBooks] = useState([])
 
-    const editBookById = (id, title) =>{
+    const fetchBooks = async()=>{
+        const response = await axios.get('http://localhost:3001/books')
+        setBooks(response.data)
+    }
+
+    useEffect(()=>{
+        fetchBooks()
+    }, [])
+
+    const editBookById = async(id, title) =>{
+        const response = await axios.put(`http://localhost:3001/books/${id}`,{
+            title:title
+        })
+        console.log(response)
         const updatedBooks = books.map((book) =>{
             if(book.id === id){
-                return {...book, title:title}
+                return {...book, ...response.data}
             }
             return book
         })
         setBooks(updatedBooks)
     }
 
-    const deleteBookById = (id) =>{
+    const deleteBookById = async(id) =>{
+
+        await axios.delete(`http://localhost:3001/books/${id}`)
+
         const updatedBooks = books.filter((book)=>{
             return book.id !== id
         })
         setBooks(updatedBooks)
     }
 
-    const createBook= (title) =>{
+    const createBook = async(title) => {
+
+
+        const response = await axios.post('http://localhost:3001/books',{
+            title
+        })
+        
         if(title){
             const updatedBooks= [
                 ...books,
-                {id: Math.floor(Math.random()*99999), title:title}
+                response.data
             ]
             setBooks(updatedBooks)
-        }
-
+        } 
     }
 
     return (
